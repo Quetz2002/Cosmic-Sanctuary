8c4c90a275bb850b52cbbc8e6a65f19371efe612
diklikr <inaki.giralt@saei.mx>
2026-05-19 14:50:39 -0600

    v:0.1.2
    water shader y menu de pausa

---
+++ Assets/Scripts/MenuScripts/Buttons.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Buttons : MonoBehaviour
+{
+    public void BackToMenu()
+    {
+        //load the menu scene
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    public void StartGame()
+        {
+            //load the game scene
+            SceneManager.LoadScene("Nave");
+    }
+}
